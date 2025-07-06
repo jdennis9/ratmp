e67158c6e11ed695c90b00ce2decb4f7d4a7120a
Jamie Dennis <jdennis9@proton.me>
2025-07-06 13:13:48 +1200

    Convert odin-imgui to submodule

--- src/thirdparty/odin-imgui/imconfig.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// @CONFIGURE:
-// This file can be filled with config lines in the same way as imconfig.h!
-// These will be used in compilation, and will be written into the bindings
-// However this support is _VERY VERY_ early and will probably go kablooey!
