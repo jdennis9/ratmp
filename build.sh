7dd8d9d2aa32ffeabd890fa9f2fe532dd7496e9f
jdennis9 <jdennis9@proton.me>
2025-12-22 15:08:46 +1300

    Change build directory to out from out/debug on Linux

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ args=$@
 fi
 
 
-odin build src/main -vet-shadowing $collections -out:out/debug/ratmp $args -show-timings \
+odin build src/main -vet-shadowing $collections -out:out/ratmp $args -show-timings \
 	-extra-linker-flags:"-no-pie -lfreetype $(pkgconf --libs gtk+-3.0) \
 	$(pkgconf --libs libavcodec) \
 	$(pkgconf --libs libavformat) \
