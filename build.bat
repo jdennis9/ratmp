768c3e50799bbf648b08b5fd1225cf45cad0eb02
Jamie Dennis <jdennis9@proton.me>
2025-12-22 15:25:56 +1300

    Change default output folder to out from out/debug on Windows

--- build.bat
+++ build.bat
@@ -8,6 +8,6 @@ IF "%~1" == "" (
 	set args=%*
 )
 
-set cmdline=odin build src/main -vet-shadowing %collections% -out:out/debug/RATMP.exe %args% -show-timings -resource:src/resources.rc -extra-linker-flags:"lib\\*.lib"
+set cmdline=odin build src/main -vet-shadowing %collections% -out:out/RATMP.exe %args% -show-timings -resource:src/resources.rc -extra-linker-flags:"lib\\*.lib"
 echo %cmdline%
 %cmdline%
